7c38158e99bee007f640408663ff214c561f19ec
swang94 <wangmaple2007@gmail.com>
2018-11-08 11:51:18 -0800

    authentication

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -3,4 +3,5 @@ export class User {
     name: string;
     password: string;
     role: string;
+    token: string;
 }
--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
+  private nodeUrl : string = "";
   constructor(private http : HttpClient) { }
 
   login(username: string, password: string) {
+    return this.http.post(`${this.nodeUrl}/users/authenticate`, { username: username, password: password })
+    .pipe(map(user => {
+        // login successful if there's a jwt token in the response; && user.token
+        if (user ) {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+        }
 
+        return user;
+    }));
   }
   logout() {
     localStorage.removeItem('currentUser');
