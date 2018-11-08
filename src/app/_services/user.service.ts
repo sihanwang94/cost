4f5bf25f767027f62f7e52ea26d747315e5e7ea7
swang94 <wangmaple2007@gmail.com>
2018-11-08 11:58:13 -0800

    user Service

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../_models';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,25 @@ import { HttpClient } from 'selenium-webdriver/http';
 export class UserService {
 
   constructor(private http : HttpClient) { }
+  
+  getAll() {
+    return this.http.get<User[]>(`/users`);
+  }
 
+  getById(id: number) {
+      return this.http.get(`/users/` + id);
+  }
+
+  register(user: User) {
+      return this.http.post(`/users/register`, user);
+  }
+
+  update(user: User) {
+      return this.http.put(`/users/` + user.id, user);
+  }
+
+  delete(id: number) {
+      return this.http.delete(`/users/` + id);
+  }
   
 }
